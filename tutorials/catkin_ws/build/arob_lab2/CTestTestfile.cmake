5c4d97d1563a1defcd8b3fcb98a3ff6fbdb29a47
viictorgallardo <801159@unizar.es>
2023-10-22 18:52:50 +0200

    Cambiando version

--- tutorials/catkin_ws/build/arob_lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arob/catkin_ws/src/arob_lab2
-# Build directory: /home/arob/catkin_ws/build/arob_lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tutorials/catkin_ws/build/arob_lab2/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-larob_lab2".split(';') if "-larob_lab2" != "" else []
-PROJECT_NAME = "arob_lab2"
-PROJECT_SPACE_DIR = "/home/arob/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
